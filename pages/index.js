86430de9fcb965d0f17eb9db8c6d9955993bd94c
Jonathan <jonicgp97@gmail.com>
2020-09-15 11:57:31 -0500

    Display the books

--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,28 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
-export default function Home() {
+export default function Home({ books }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>ASD Devotionals</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          ASD Devotionals
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          {books.map((book) => (
+            <a href={`/book/${book._id}`} className={styles.card}>
+              <h3>{book.title}</h3>
+              <img alt="Book portrait" src={book.image} className={styles.image_book}></img>
+            </a>
+          ))}
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
@@ -61,5 +37,24 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const token = process.env.API_TOKEN;
+  const res = await fetch('https://devotionals-api.herokuapp.com/api/v1/books', 
+  {
+    headers: {
+      'Authorization': `Bearer ${token}`,
+    },
+  });
+  const books = await res.json();
+
+  console.log(books);
+
+  return {
+    props: {
+      books: books,
+    },
+  }
 }
